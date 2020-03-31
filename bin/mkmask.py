d0afc00cc283a972768a854c0a5f39864900885a
nmik <michela.negro@to.infn.it>
2020-03-31 11:48:18 -0400

    minor

--- bin/mkmask.py
+++ bin/mkmask.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python                                                          #
 #                                                                              #
 # Autor: Michela Negro, University of Torino.                                  #
