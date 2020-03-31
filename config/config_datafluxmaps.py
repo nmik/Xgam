f9b6995b3481ca5ccaa7b9f3716faef78c89b07c
nmik <michela.negro@to.infn.it>
2020-03-31 16:40:17 -0400

    routine to make the flux maps

---
+++ config/config_datafluxmaps.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python                                                          #
+#                                                                              #
+# Autor: Michela Negro, GSFC/CRESST/UMBC    .                                  #
+# On behalf of the Fermi-LAT Collaboration.                                    #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation; either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+import os
+from Xgam import X_OUT
+
+IN_LABELS_LIST = ['w9w50_SV_t56', 'w51w100_SV_t56'] 
+
+FORE_FILES_LIST = [os.path.join(X_OUT,'fits/gll_iem_v07_hp512_407.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_528.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_687.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_893.fits'),
+			       os.path.join(X_OUT,'fits/gll_iem_v07_hp512_1161.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_1509.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_1961.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_2549.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_3313.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_4305.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_7273.fits'),
+                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_12284.fits')]
+FORE_LABEL = 'glliemv07'                   
+OUT_LABEL = '100weeks_SV_t56'
+
+BINNING_LABEL = '2binsTEST'
+MICRO_BINS_FILE = os.path.join(X_OUT, 'ebinning.fits')
+MACRO_BINS = [(27,32),(33, 38)]
+
+POWER_LOW_INDEX = 2.30
+
+MASK_FILE = os.path.join(X_OUT, 'fits/Mask_gp25_4fgl_extsrc.fits')  #'None' if no mask is needed
+MASK_LABEL = 'mymask'
