25ecc7722b0d9afd2cabca07ab3c4eae1f712168
nmik <michela.negro@to.infn.it>
2020-06-01 17:18:31 -0400

    changed header

--- config/config_datafluxmaps.py
+++ config/config_datafluxmaps.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python                                                          #
 #                                                                              #
 # Autor: Michela Negro, GSFC/CRESST/UMBC    .                                  #
-# On behalf of the Fermi-LAT Collaboration.                                    #
 #                                                                              #
 # This program is free software; you can redistribute it and/or modify         #
 # it under the terms of the GNU General Public License as published by         #
