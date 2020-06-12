c9770d02433d3489a54e795c7c31f13583d6e1cc
nmik <michela.negro@to.infn.it>
2020-06-12 14:33:54 -0400

    minor

--- config/config_datafluxmaps.py
+++ config/config_datafluxmaps.py
@@ -12,29 +12,25 @@
 import os
 from Xgam import X_OUT
 
-IN_LABELS_LIST = ['w9w50_SV_t56', 'w51w100_SV_t56']
+OUT_LABEL = '12years_SV_t56'
 
-FORE_FILES_LIST = [os.path.join(X_OUT,'fits/gll_iem_v07_hp512_407.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_528.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_687.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_893.fits'),
-			       os.path.join(X_OUT,'fits/gll_iem_v07_hp512_1161.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_1509.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_1961.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_2549.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_3313.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_4305.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_7273.fits'),
-                   os.path.join(X_OUT,'fits/gll_iem_v07_hp512_12284.fits')]
-FORE_LABEL = 'glliemv07'
-OUT_LABEL = '100weeks_SV_t56'
+IN_LABELS_LIST = ['1yr_SV_t56', '2yr_SV_t56',
+			      '3yr_SV_t56', '4yr_SV_t56',
+			      '5yr_SV_t56', '6yr_SV_t56',
+			      '7yr_SV_t56', '8yr_SV_t56',
+			      '9yr_SV_t56', '10yr_SV_t56',
+			      '11yr_SV_t56', '12yr_SV_t56']
 
-BINNING_LABEL = '2binsTEST'
+# Activated with --foresub True
+FORE_FILES_LIST = os.path.join(X_OUT, 'fits/gll_iem_v06_hp512_list.txt') 
+FORE_LABEL = 'glliemv06'
+
+BINNING_LABEL = 'mybins'
 MICRO_BINS_FILE = os.path.join(X_OUT, 'ebinning.fits')
-MACRO_BINS = [(27,32),(33, 38)]
+MACRO_BINS = [(21,24),(25,32),(33,41),(42,50),(51,66),(67,86)]
 
-IGRB_FILE = os.path.join(X_OUT, 'data/iso_P8R3_SOURCEVETO_V2_v1.txt') #None if foreground subtraction is not performed
+IGRB_FILE = os.path.join('config/IGRB_2015.txt') #None if foreground subtraction is not performed
 BINCALC = 'CENTER' #CENTER or EDGE, matching dataset selection
 
-MASK_FILE = os.path.join(X_OUT, 'fits/Mask_gp25_4fgl_extsrc.fits')  #'None' if no mask is needed
-MASK_LABEL = 'mymask'
+MASK_FILE = os.path.join(X_OUT, 'fits/Masksmart_hp512_gp25_src4fgl_testbins_list.txt')  #'None' if no mask is needed
+MASK_LABEL = 'SMgp25src4fgl'
