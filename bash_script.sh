4cc628be81acc93d6664625a8c5508fe81c08ec2
nmik <michela.negro@to.infn.it>
2021-04-29 18:11:04 -0400

    added explicitly a galactic plan cut in the example of mksmartmask, even if it's the same as the default value

--- bash_script.sh
+++ bash_script.sh
@@ -4,6 +4,6 @@ python /run_xgam/Xgam/bin/mkdataselection.py --config=/run_xgam/home/CONFIG/conf
 
 python /run_xgam/Xgam/bin/mkforeground.py --infile=/run_xgam/Xgam/fits/gll_iem_v07.fits --nsideout=1024
 
-python /run_xgam/Xgam/bin/mksmartmask.py --config=/run_xgam/home/CONFIG/config_datafluxmaps.py --srccat=/run_xgam/Xgam/fits/gll_psc_v20.fit --srcextcat=/run_xgam/Xgam/fits/gll_psc_v20.fit --irfs=P8R3_SOURCEVETO_V2 --evtype=3 --ltfile=/run_xgam/home/output/output_gtltcube/tag_outofltcube.fits --outflabel=tag
+python /run_xgam/Xgam/bin/mksmartmask.py --config=/run_xgam/home/CONFIG/config_datafluxmaps.py --gpcut 25 --srccat=/run_xgam/Xgam/fits/gll_psc_v20.fit --srcextcat=/run_xgam/Xgam/fits/gll_psc_v20.fit --irfs=P8R3_SOURCEVETO_V2 --evtype=3 --ltfile=/run_xgam/home/output/output_gtltcube/tag_outofltcube.fits --outflabel=tag
 
 python /run_xgam/Xgam/bin/mkdatafluxmaps.py --config=/run_xgam/home/CONFIG/config_datafluxmaps.py
